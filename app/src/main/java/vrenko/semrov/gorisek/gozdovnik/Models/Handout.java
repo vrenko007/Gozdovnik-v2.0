2ab2f9dc398de3b36134347719e858c8b9363e09
Matic Vrenko <matic.vrenko@gmail.com>
2015-01-07 12:03:58 +0100

    Implementiran Handout.java

--- app/src/main/java/vrenko/semrov/gorisek/gozdovnik/Models/Handout.java
+++ app/src/main/java/vrenko/semrov/gorisek/gozdovnik/Models/Handout.java
@@ -1,6 +1,8 @@
 package vrenko.semrov.gorisek.gozdovnik.Models;
 
 import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 
 import java.io.File;
@@ -12,4 +14,20 @@ import java.io.File;
 @ParseClassName("Handout")
 public class Handout extends ParseObject {
 
+    public String getName() {
+        return getString("name");
+    }
+
+    public byte[] getFileAsByteArray() {
+        try {
+            return getParseFile("document").getData();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ParseFile getFileAsParseFile() {
+        return getParseFile("document");
+    }
 }
