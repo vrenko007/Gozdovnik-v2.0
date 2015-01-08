892070590fe74ebc38bcf74c96251c9b77e64fdf
Jure <jure.semrov@outlook.com>
2015-01-08 18:42:14 +0100

    S klikom na gumb "open pdf" lahko testirate, če vam deluje branje pdf-jev. Lokacija pdf-ja mora biti ista kot je navedena  v String path v HomeActivity.java . To možnost lahko kaseje kadarkoli kdorkoli zbriše.

--- app/src/main/java/vrenko/semrov/gorisek/gozdovnik/HomeActivity.java
+++ app/src/main/java/vrenko/semrov/gorisek/gozdovnik/HomeActivity.java
@@ -1,17 +1,26 @@
 package vrenko.semrov.gorisek.gozdovnik;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.artifex.mupdfdemo.MuPDFActivity;
 import com.parse.ParseUser;
+
+import java.io.File;
 
 
 public class HomeActivity extends ActionBarActivity {
+
+    // za testne namene
+    Button btnPdf;
+    private final static String path =   "/sdcard/Download/test.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,29 @@ public class HomeActivity extends ActionBarActivity {
                 ParseUser.logOut();
                 if(ParseUser.getCurrentUser() == null){
                     startActivity(new Intent(HomeActivity.this, LoginDispatchActivity.class));
+                }
+            }
+        });
+
+        // za testne namene
+        btnPdf = (Button) findViewById(R.id.btnPdf);
+        btnPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File f = new File(path);
+                if(f.exists())
+                {
+                    Intent intent = new Intent(v.getContext(), MuPDFActivity.class);
+                    intent.setAction("android.intent.action.VIEW");
+                    intent.setData(Uri.fromFile(f));
+                    intent.putExtra("FileName", path);
+                    startActivity(intent);
+
+
+                }
+                else
+                {
+                    Toast.makeText(v.getContext(),"Ne najdem test.pdf",Toast.LENGTH_LONG).show();
                 }
             }
         });
